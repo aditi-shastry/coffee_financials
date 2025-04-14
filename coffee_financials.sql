55a5388046022200f2a39bdae05b6dffa63e2333
aditi-shastry <88719315+aditi-shastry@users.noreply.github.com>
2025-04-14 11:21:08 -0700

    Update and rename cofee_financials.sql to coffee_financials.sql

--- coffee_financials.sql
+++ coffee_financials.sql
@@ -1,22 +1,23 @@
-#find weekly revenue and cost of goods sold of each item 
+/* find weekly revenue and cost of goods sold of each item */
 
 #the order_details CTE finds weekly revenue per item 
 WITH order_details AS (
-SELECT orders.item_id, 
-items.item_name,
-items.item_size,
-COUNT(orders.quantity) AS total_ordered,
-items.item_price AS price
-FROM orders
 
-LEFT OUTER JOIN items
- ON orders.item_id = items.item_id
+SELECT od.item_id, 
+i.item_name, 
+i.item_size, 
+SUM(od.quantity) AS total_ordered, 
+i.item_price AS price
+FROM orders AS od
 
-GROUP BY orders.item_id, items.item_name, items.item_size, items.item_price
-ORDER BY items.item_id
+INNER JOIN (SELECT item_id, item_name, item_size, item_price FROM items) AS i
+ ON od.item_id = i.item_id 
+ 
+GROUP BY od.item_id, i.item_name, i.item_size, i.item_price 
+ORDER BY od.item_id
 ),
 
-#the cost_of_ingredients CTE and subquery finds cost of goods sold (COGS) per item 
+/* the cost_of_ingredients CTE and subquery find cost of goods sold (COGS) per item */
 cost_of_ingredients AS (
 SELECT ingre_details.item_name,
 ingre_details.item_size,
@@ -29,6 +30,9 @@ items.item_size,
 r.recipe_id,
 r.ing_id, 
 i.ing_name,
+r.quantity,
+i.ing_price,
+i.ing_weight,
 ROUND((r.quantity * 1.0 * (i.ing_price*1.0/i.ing_weight)),3) AS ing_costs
 FROM recipe AS r
 
@@ -38,13 +42,22 @@ LEFT OUTER JOIN ingredients AS i
 RIGHT OUTER JOIN items 
  ON r.recipe_id = items.sku
  
-GROUP BY items.item_name, items.item_size, r.recipe_id, r.ing_id, i.ing_name, ROUND((r.quantity * 1.0 * (i.ing_price*1.0/i.ing_weight)),3)
+GROUP BY items.item_name, 
+items.item_size, 
+r.recipe_id, 
+r.ing_id, 
+i.ing_name, 
+r.quantity, 
+i.ing_price, 
+i.ing_weight 
 ORDER BY items.item_name ASC) AS ingre_details
-
 
 GROUP BY ingre_details.item_name, ingre_details.item_size, ingre_details.recipe_id)
 
-#the item id, name, size, weekly revenue, and weekly cost are displayed 
+ 
+/*the item id, name, size, total ordered, sales price, cost per item
+weekly revenue, and weekly cost are displayed */
+ 
 SELECT od.item_id,
 od.item_name,
 od.item_size,
@@ -55,10 +68,15 @@ CONCAT('$',ROUND((od.total_ordered * 1.0 * od.price),2)) AS weekly_item_revenue,
 CONCAT('$',ROUND(od.total_ordered * 1.0 * coi.costs_per_item,2)) AS weekly_item_cost
 FROM order_details AS od
 
-LEFT OUTER JOIN cost_of_ingredients AS coi
- ON od.item_name = coi.item_name 
+INNER JOIN cost_of_ingredients AS coi
+ ON od.item_name = coi.item_name AND od.item_size = coi.item_size
  
-GROUP BY od.item_id, od.item_name, od.item_size, od.total_ordered, od.price, coi.costs_per_item, CONCAT('$',ROUND((od.total_ordered * 1.0 * od.price),2)), CONCAT('$',ROUND(od.total_ordered * 1.0 * coi.costs_per_item,2))
+GROUP BY od.item_id, 
+od.item_name, 
+od.item_size, 
+od.total_ordered, 
+od.price, 
+coi.costs_per_item 
 ORDER BY od.item_id, od.item_name;
 
 
